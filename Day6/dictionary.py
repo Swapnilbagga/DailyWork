46d27aa59762af3fb6220626d05245098f42436b
Swapnilbagga <109497191+Swapnilbagga@users.noreply.github.com>
2022-07-26 10:37:24 +0530

    First Commit

---
+++ Day6/dictionary.py
@@ -0,0 +1,4 @@
+s={10,2,30,4}
+print(s)
+print(type(s))
+print(len(s))
---
+++ Day6/palindrome.py
@@ -0,0 +1,6 @@
+a=input("Enter the string : ")
+b=a[::-1]
+if(a==b):
+    print("String is palindrome.")
+else:
+    print("String is not palindrome.")
