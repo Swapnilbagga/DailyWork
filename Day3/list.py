f2c4f51d6dae38fe09c3d4e9257a30a2c92c1a1f
Swapnilbagga <109497191+Swapnilbagga@users.noreply.github.com>
2022-07-26 10:36:43 +0530

    First commit

---
+++ Day3/list.py
@@ -0,0 +1,5 @@
+l=[1,2,"Hello","Welcome",10.5]
+print(l)
+print(len(l))
+l.append(50)
+print(l)
---
+++ Day3/sum.py
@@ -0,0 +1,5 @@
+def sum(a,b):
+    print(a+b)  
+a=int(input("Enter first number :"))
+b=int(input("Enter first number :"))
+sum(a,b)
