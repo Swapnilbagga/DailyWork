41ee3342bddf5b0d720178d7dbaf19bf30b33b36
Swapnilbagga <109497191+Swapnilbagga@users.noreply.github.com>
2022-07-27 20:10:04 +0530

    Update registration-form-with-gui.py

--- Day9/registration-form-with-gui.py
+++ Day9/registration-form-with-gui.py
@@ -1,38 +1,55 @@
-from tkinter import*
-root = Tk()
-root.geometry('500x500')
-root.title("Registration Form")
-
-label_0 = Label(root, text="Registration form",width=20,font=("bold", 20))
-label_0.place(x=90,y=53)
-
-
-label_1 = Label(root, text="FullName",width=20,font=("bold", 10))
-label_1.place(x=80,y=130)
-
-entry_1 = Entry(root)
-entry_1.place(x=240,y=130)
-
-label_2 = Label(root, text="Email",width=20,font=("bold", 10))
-label_2.place(x=68,y=180)
-
-entry_2 = Entry(root)
-entry_2.place(x=240,y=180)
-
-label_3 = Label(root, text="Gender",width=20,font=("bold", 10))
-label_3.place(x=70,y=230)
-var = IntVar()
-Radiobutton(root, text="Male",padx = 5, variable=var, value=1).place(x=235,y=230)
-Radiobutton(root, text="Female",padx = 20, variable=var, value=2).place(x=290,y=230)
-
-label_4 = Label(root, text="Age:",width=20,font=("bold", 10))
-label_4.place(x=70,y=280)
+from tkinter import *
+from tkinter import messagebox as msgbx
+from matplotlib.pyplot import text
+import mysql.connector
+window = Tk()
+window.title("Registration Form")
+window.geometry('400x400')
+window.config(bg='cadetblue')
 
 
-entry_2 = Entry(root)
-entry_2.place(x=240,y=280)
+def reset():
+    firstname.set(" ")
+    lastname.set(" ")
+    emailid.set(" ")
+    contact_no.set(None)
 
-Button(root, text='Submit',width=20,bg='brown',fg='white').place(x=180,y=380)
-# it is use for display the registration form on the window
-root.mainloop()
-print("registration form  seccussfully created...")
+
+def register():
+    conn=mysql.connector.connect(host='127.0.0.1',port='3306',user='root',password='root',database='database')
+    mycursor=conn.cursor()
+    fname=a1.get()
+    lname=b1.get()
+    email=c1.get()
+    contact=e1.get()
+    mycursor.execute("insert into register values(%s,%s,%s,%s)",(fname,lname,email,contact))
+    msgbx.showinfo("Registration","Submitted")
+    conn.commit()
+
+
+d= Label(window, text="Registration form",font=("bold", 15),bg='cadetblue').grid(row = 0,column = 0)
+a = Label(window ,text = "First Name:",padx=10,pady=10,bg='cadetblue',font=("bold", 15)).grid(row = 1,column = 0)
+b = Label(window ,text = "Last Name:",padx=10,pady=10,bg='cadetblue',font=("bold", 15)).grid(row = 2,column = 0)
+c = Label(window ,text = "Email Id:",padx=10,pady=10,bg='cadetblue',font=("bold", 15)).grid(row = 3,column = 0)
+e = Label(window ,text = "Contact Number:",padx=10,pady=10,bg='cadetblue',font=("bold", 15)).grid(row = 4,column = 0)
+btn1  = Button(window ,text="Submit",command=register,width=15, bg= 'green',fg='yellow',pady=10,).grid(row=5,column=0)
+btn2= Button(window,text="Reset",width=15, bg= 'green',fg='yellow',pady=10,command=reset).grid(row=5,column=1)
+
+
+
+firstname = StringVar()
+lastname = StringVar()
+emailid= StringVar()
+contact_no = IntVar()
+
+a1 = Entry(window,textvariable=firstname)
+a1.grid(row = 1,column = 1)
+b1 = Entry(window,textvariable=lastname)
+b1.grid(row = 2,column = 1)
+c1 = Entry(window,textvariable=emailid)
+c1.grid(row = 3,column = 1)
+e1 = Entry(window,textvariable=contact_no)
+e1.grid(row = 4,column = 1)
+window.mainloop()
+
+
